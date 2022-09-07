aad5789a9ad3f8b306ae97681109155566743d1e
seong0210 <dlgustjd9566@gmail.com>
2022-09-07 16:13:41 +0900

    delete Form태그로 만들기

--- src/main/java/site/metacoding/red/web/BoardsController.java
+++ src/main/java/site/metacoding/red/web/BoardsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.red.domain.boards.Boards;
 import site.metacoding.red.domain.boards.BoardsDao;
 import site.metacoding.red.domain.users.Users;
 import site.metacoding.red.web.dto.request.boards.WriteDto;
@@ -25,6 +26,37 @@ public class BoardsController {
 	private final BoardsDao boardsDao;
 	// @PostMapping("/boards/{id}/delete")
 	// @PostMapping("/boards/{id}/update")
+	
+	
+	//1. 만약 50번을 id로 주고 삭제 한다고 치자.
+	// -> 50번이 없어도 0행이 삭제 되었다고 실행이 된다. 
+	// 이것을 체크 해주어야 한다. 
+	// 트랜젝션 관리를 위해서 영속화를 한번 해주는것이 좋다 !
+	@PostMapping("/boards/{id}/delete")
+	public String deleteBoards(@PathVariable Integer id) {
+		
+		Users principal =  (Users)session.getAttribute("principal");
+		//1. 영속화 (트랜젝션 관리를 위해 해주는것이 좋다)
+		Boards boardsPs = boardsDao.findById(id);
+		// 로그인 인증 체크
+		if(principal == null) {
+			System.out.println("로그인 해주세요.");
+			return "redirect:/";
+		}
+		// 권한 체크 (principal.getId가 boardsPs의 users아이디가 동일 해야한다. 
+		
+		if(principal.getId() != boardsPs.getUsersId()) {
+			System.out.println("삭제 권한이 없습니다.");
+			return "redirect:/";
+		}
+
+		if(boardsPs == null) {// if는 비정상 로직을 타게해서 걸러내는 필터 역할로 써주는 것이 좋다 . 
+			return "redirect:/boards/" + id;
+		}
+		// null값 체크 했으므로 아래 코드에서 null이 들어갈 일은 없다. 
+		boardsDao.delete(id);
+		return "redirect:/";
+	}
 	
 	@PostMapping("/boards")
 	public String writeBoards(WriteDto writeDto) {
--- src/main/webapp/WEB-INF/views/boards/detail.jsp
+++ src/main/webapp/WEB-INF/views/boards/detail.jsp
@@ -1,16 +1,26 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
 <%@ include file="../layout/header.jsp"%>
 
 <div class="container">
 	<br /> <br />
+	
 	<div>
 		<h3>${boards.title}</h3>
 	</div>
-	<hr/>
+	<hr />
 
 	<div>${boards.content}</div>
+<div class="d-flex">
+
+		<form>
+			<button class="btn btn-outline-warning">수정 하기</button>
+		</form>
+
+		<form action="/boards/${boards.id}/delete" method="post">
+			<button class="btn btn-outline-danger">삭제</button>
+		</form>
+	</div>
 
 
 </div>
